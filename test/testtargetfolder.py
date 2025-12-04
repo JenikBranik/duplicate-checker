02837731be073da14b4274fb8ec5380e9491ad55
JenikBranik <honza.vavrouseke@gmail.com>
2025-12-04 19:48:58 +0100

    Adding a test

--- test/testtargetfolder.py
+++ test/testtargetfolder.py
@@ -27,6 +27,22 @@ class TestTargetFolder(unittest.TestCase):
         with self.assertRaises(FileNotFoundError):
             TargetFolder(path)
 
+    def test_path_is_file_raises_error(self):
+        """
+        Path exist but is not a file
+        """
+        with tempfile.NamedTemporaryFile() as tmp_file:
+            with self.assertRaises(FileNotFoundError):
+                TargetFolder(tmp_file.name)
+
+    def test_invalid_input_type(self):
+        """
+        Test if input aint string
+        """
+        with self.assertRaises(TypeError):
+            TargetFolder(12345)
+        with self.assertRaises(TypeError):
+            TargetFolder(None)
 
 if __name__ == '__main__':
     unittest.main()
