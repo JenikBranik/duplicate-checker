fe1ec071fb8c031c8cb7f5f5816ce2fe0f448f3e
JenikBranik <honza.vavrouseke@gmail.com>
2025-12-18 15:50:25 +0100

    Added tests

--- test/testtargetfolder.py
+++ test/testtargetfolder.py
@@ -1,22 +1,30 @@
 import unittest
 import tempfile
 import shutil
-
+import os
 from src.utils.targetfolder import TargetFolder
 
 class TestTargetFolder(unittest.TestCase):
+
+    def setUp(self):
+        """Set up test fixtures"""
+        self.temp_dir = tempfile.mkdtemp()
 
     def test_valid_folder_initialization(self):
         """
         Valid input test
         """
-        temp_dir = tempfile.mkdtemp()
+        target = TargetFolder(self.temp_dir)
+        self.assertEqual(target.get_target_folder, self.temp_dir)
 
-        try:
-            target = TargetFolder(temp_dir)
-            self.assertEqual(target.get_target_folder, temp_dir)
-        finally:
-            shutil.rmtree(temp_dir)
+    def test_valid_folder_with_subdirectories(self):
+        """
+        Test initialization with folder containing subdirectories
+        """
+        subdir = os.path.join(self.temp_dir, "subdir")
+        os.makedirs(subdir)
+        target = TargetFolder(self.temp_dir)
+        self.assertEqual(target.get_target_folder, self.temp_dir)
 
     def test_non_existent_folder_raises_error(self):
         """
@@ -27,22 +35,35 @@ class TestTargetFolder(unittest.TestCase):
         with self.assertRaises(FileNotFoundError):
             TargetFolder(path)
 
-    def test_path_is_file_raises_error(self):
+    def test_invalid_input_type_none(self):
         """
-        Path exist but is not a file
+        Test if input is None
         """
-        with tempfile.NamedTemporaryFile() as tmp_file:
-            with self.assertRaises(FileNotFoundError):
-                TargetFolder(tmp_file.name)
-
-    def test_invalid_input_type(self):
-        """
-        Test if input aint string
-        """
-        with self.assertRaises(TypeError):
-            TargetFolder(12345)
         with self.assertRaises(TypeError):
             TargetFolder(None)
 
-if __name__ == '__main__':
-    unittest.main()
+    def test_invalid_input_type_dict(self):
+        """
+        Test if input is dictionary
+        """
+        with self.assertRaises(TypeError):
+            TargetFolder({"path": self.temp_dir})
+
+    def test_empty_string_raises_error(self):
+        """
+        Test empty string input
+        """
+        with self.assertRaises(FileNotFoundError):
+            TargetFolder("")
+
+    def test_set_target_folder_validates_path(self):
+        """
+        Test that set_target_folder validates the path
+        """
+        target = TargetFolder(self.temp_dir)
+        new_valid_dir = tempfile.mkdtemp()
+        try:
+            target.set_target_folder(new_valid_dir)
+            self.assertEqual(target.get_target_folder, new_valid_dir)
+        finally:
+            shutil.rmtree(new_valid_dir)
